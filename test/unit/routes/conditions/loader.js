d249da2cf1555913c5b949a2327dfda10d61a0a7
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2015-07-08 21:59:32 +0900

    Experimental implementation of the routing conditions loader which loads routing conditions from a JSON object

---
+++ test/unit/routes/conditions/loader.js
@@ -0,0 +1,71 @@
+(function () {
+    /*globals describe:false, it:false*/
+    'use strict';
+
+    var expect = require('chai').expect;
+
+    var Factory = require('../../../../lib/plugins/rollout/factory');
+    var Loader = require('../../../../lib/routes/conditions/loader').Loader;
+
+    var factory = Factory.fromDependencies({
+        redisOptions: {
+            host: '127.0.0.1',
+            port: 6379
+        },
+        pathMatchingOptions: {
+            prefix: 'rollout_paths:'
+        },
+        includedInSegmentOptions: {
+            cookie: '_myapp_session_id',
+            maxSegmentId: 75
+        }
+    });
+
+    var config = {
+        and: {
+            //module: {
+            //    name: './routes/conditions/and'
+            //},
+            conditions: [
+                {
+                    pathMatches: {
+                        //module: {
+                        //    name: "./routes/conditions/match_path_defined_in"
+                        //},
+                        prefix: "overrode_prefix"
+                    }
+                },
+                {
+                    includedInSegment: {
+                        //module: {
+                        //    name: "./routes/conditions/percentage_of_segment"
+                        //},
+                        cookie: "overrode_cookie",
+                        maxSegmentId: 77
+                    }
+                },
+                {
+                    includedInSegment: {}
+                }
+            ]
+        }
+    };
+
+    describe('Loader', function () {
+        describe('#load', function () {
+            it('returns true when all the conditions resulted in truthy values', function () {
+                var req = {req:{name:"foo"}};
+                var loader = new Loader(factory);
+                var cond = loader.load(config);
+
+                expect(cond.conditions[0].options.prefix).to.eql('overrode_prefix');
+                expect(cond.conditions[1].options.cookie).to.eql('overrode_cookie');
+                expect(cond.conditions[1].options.maxSegmentId).to.eql(77);
+                expect(cond.conditions[2].options.cookie).to.eql('_myapp_session_id');
+                expect(cond.conditions[2].options.maxSegmentId).to.eql(75);
+
+                loader.destructor();
+            });
+        });
+    });
+})();
